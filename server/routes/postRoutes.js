cf2aa62ea40c3ab8f6d7972afe85822776166c8a
Vilma Ikonen <vilma@Haukka.local>
2021-03-16 14:10:35 +0200

    middleware for user authentification process and using middleware in like, delete etc. routes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,24 +1,26 @@
 import express from 'express';
 
 import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/postControllers.js'
+import auth from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
 // Routes
 
-// All routes in posts.js are reached via localhost:5000/posts, not localhost:5000/, as the prefix of posts is set in index.js to '/posts
+// All routes in posts.js are reached via localhost:5000/posts, not localhost:5000/, as the prefix of posts is set in index.js to '/posts'
+// All other routes, exept getAll, require authentication, ie use auth middleware
 router.get('/', getPosts); 
 
-router.post('/', createPost);
+router.post('/', auth, createPost);
 
 // For modifying existing post:
-router.patch('/:id', updatePost);
+router.patch('/:id', auth, updatePost);
 
 // Like (similar to updating. Here the no. of likes is updated):
-router.patch('/:id/likePost', likePost);
+router.patch('/:id/likePost', auth, likePost);
 
 // For deleting a post:
-router.delete('/:id', deletePost)
+router.delete('/:id', auth, deletePost)
 
 
 export default router;
