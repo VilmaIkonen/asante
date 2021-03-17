a17f608efb0c9a3b1372e729f5ebb8189fc8f451
Vilma Ikonen <vilma@Haukka.local>
2021-03-17 16:55:05 +0200

    axios interceptor for sending token data to backend for verification

--- client/src/api/indexAPI.js
+++ client/src/api/indexAPI.js
@@ -4,6 +4,15 @@ import axios from 'axios';
 // axios instance and base URL:
 const API = axios.create({baseURL: 'http://localhost:5000'});
 
+// interceptor is axios's equivalent to middleware in Express, function will be called before all request below:
+// token sent back to backend so that backend middleware can verify that user is signed in.
+API.interceptors.request.use((req) => {
+  if(localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`; // Bearer auth = auth type with token
+  }
+  return req;
+})
+
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts', newPost); // send where(?) and what(?)=payload
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
