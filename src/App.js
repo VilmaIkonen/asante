059f0d37b247b4f9924ba178c0c69db0e871f956
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-04-10 13:18:39 +0300

    Redundant imports deleted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
