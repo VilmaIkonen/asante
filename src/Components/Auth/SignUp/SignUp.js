53c1e185665fa600a14e0779b7e15b9634547182
VilmaIkonen <vilma.r.ikonen@gmail.com>
2021-05-10 15:30:50 +0300

    Simple form validation added to sign in and sign up forms

--- src/Components/Auth/SignUp/SignUp.js
+++ src/Components/Auth/SignUp/SignUp.js
@@ -7,6 +7,7 @@ import { signup } from '../../../actions/authActions'
 import InputLarge from '../../Inputs/InputLarge';
 import LargeButton from '../../Buttons/LargeButton/LargeButton'
 import useStyles from './stylesSignUp';
+import Validator from '../../../services/validator'
 
 const formInitialState = {firstname: '', lastname: '', email: '', password: '', confirmPassword: ''};
 
@@ -16,9 +17,24 @@ const SignUp = () => {
   const [formData, setFormData] = useState(formInitialState);
   const dispatch = useDispatch();
   const history = useHistory();
+  const [error, setError] = useState([])
+
+  const [inputs] = useState([
+    { id:'firstname', label:'First name',  name:'firstname', type:'text', autoFocus: 'true' },
+    { id:'lastname', label:'Last name',  name:'lastname', type:'text', autoFocus: 'false' },
+    { id:'email', label:'Email',  name:'email', type:'email', autoFocus: 'false' },
+    { id:'password', label:'Password',  name:'password', type:'password', autoFocus: 'false' },
+    { id:'confirmPassword', label:'Confirm password',  name:'confirmPassword', type:'password', autoFocus: 'false' }
+  ])
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setError([])
+    const validation = Validator.signUp(formData)
+    if (validation.length > 0) {
+      setError(validation)
+      return
+    }
     dispatch(signup(formData, history)) //action = signup. formData and history send with it
   }
 
@@ -32,40 +48,30 @@ const SignUp = () => {
       <Typography variant='h3'>Sign up</Typography>
       <form className={classes.form} onSubmit={handleSubmit}>  
         <Grid container>
-          <InputLarge 
-            label='First name'
-            name='firstname'
-            onChange={handleChange}
-            required
-          />
-          <InputLarge 
-            label='Last name'
-            name='lastname'
-            onChange={handleChange}
-            required
-          />      
-          <InputLarge 
-            label='Email'
-            name='email' 
+          {inputs.map((input) => (
+            <InputLarge 
+            key={input.id}
+            id={input.id}
+            label={input.label}
+            name={input.name}           
+            type={input.type}
             onChange={handleChange} 
-            type='email'
-            required
+            autoFocus={input.autoFocus}
+            error={error.includes(input.id)}
           />
-          <InputLarge 
-            label='Password'
-            name='password'
-            onChange={handleChange} 
-            type='password'
-            required
-          />
-          <InputLarge 
-            label='Confirm password'
-            name='confirmPassword' 
-            placeholder='Repeat password' 
-            onChange={handleChange} 
-            type='password'
-            required
-          />
+          ))}
+            {error && (
+              <p
+                style={{
+                    color: 'red',
+                    padding: '0',
+                    fontSize: '12px',
+                    marginBottom: '0.1rem',
+                }}
+                className="d-flex flex-row justify-content-center">
+                {error[0]}
+              </p>
+            )}         
         </Grid>           
         <LargeButton name='Sign up'/>
       </form>
